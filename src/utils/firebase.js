520edebb65993e07e49a52812b14e75f5394e78b
Harshit Singh Tomar <harshit.nitmz@gmail.com>
2023-08-15 13:20:12 +0530

    fix env variable syntax

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -7,14 +7,14 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_MEASUREMENT_ID
+  apiKey: import.meta.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.VITE_FIREBASE_AUTH_DOMAIN,
+  databseURL: import.meta.VITE_FIREBASE_DATABASE_URL,
+  projectId: import.meta.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.VITE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
