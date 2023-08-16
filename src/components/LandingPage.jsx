6130061e1e5392ac3ae1250812eb25a4f3430ade
Harshit Singh Tomar <harshit.nitmz@gmail.com>
2023-08-16 10:45:55 +0530

    fix deployment errors

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Carousel } from 'react-responsive-carousel';
+import { Carousel } from 'react-responsive-carousel/lib/js';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
 const LandingPage = () => {
