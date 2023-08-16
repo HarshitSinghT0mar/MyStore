89671106c0e366ae3c8c5d70fd9dd15f579382a6
Harshit Singh Tomar <harshit.nitmz@gmail.com>
2023-08-16 10:22:00 +0530

    fix carousel error

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,56 +1,47 @@
 import React from 'react';
-import { Carousel } from '../../node_modules/react-responsive-carousel';
 import { Link } from 'react-router-dom';
-
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
 const LandingPage = () => {
-
   return (
     <div className="ecommerce-landing-page">
       <div className="mystore-header">
-        <h1 style={{fontWeight:"bold"}}>Welcome to MyStore</h1>
+        <h1 style={{ fontWeight: "bold" }}>Welcome to MyStore</h1>
         <p>Discover the best deals on your favorite products.</p>
-       
-
       </div>
-      <section className="section-image-carousel">
-     
-        <Carousel interval={3000} autoPlay={true} infiniteLoop={true} stopOnHover={false} showThumbs={false}>
-          <div>
-            <img loading="lazy" src="./images/sale.jpg" alt="Product 1" />
- 
-          </div>
-          <div>
-            <img loading="lazy" src="./images/weekendOnly.jpg" alt="Product 2" />
       
-          </div>
-          <div>
-            <img loading="lazy" src="./images/holidayShopping.jpg" alt="Product 3" />
+<section className="section-image-carousel">
      
-          </div>
-        </Carousel>
-      </section>
-      
+     <Carousel interval={3000} autoPlay={true} infiniteLoop={true} stopOnHover={false} showThumbs={false}>
+       <div>
+         <img loading="lazy" src="./images/sale.jpg" alt="Product 1" />
+     
+       </div>
+       <div>
+         <img loading="lazy" src="./images/weekendOnly.jpg" alt="Product 2" />
+     
+       </div>
+       <div>
+         <img loading="lazy" src="./images/holidayShopping.jpg" alt="Product 3" />
+     
+       </div>
+     </Carousel>
+     </section>
       <div className="main-banner">
         <h1>Shop Now</h1>
         <p>Explore a wide range of products and find what you need.</p>
-     
-        <div className="image-section">
-          <div className="image-box">
-          
+<div className='image-section'>
+          <div className="product-image">
             <img loading="lazy" src="./images/clothing.jpg" alt="Product Image 2" />
           </div>
-          <div className="image-box">
+          <div className="product-image">
             <img loading="lazy" src="./images/jewellery.jpg" alt="Product Image 1" />
-         
           </div>
-          <div className="image-box">
+          <div className="product-image">
             <img loading="lazy" src="./images/Headphone.jpg" alt="Product Image 3" />
-          
           </div>
-         
-        </div>
-        
+          </div>
         <Link to="/Home" className="cta-button">Browse All Products</Link>
       </div>
     </div>
